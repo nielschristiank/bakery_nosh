3a82b9d6a3ce987f2cf5fab43f5dbba9ab9d182d
Scott Kwon <scottkwon@gmail.com>
2017-06-01 18:30:24 -0700

    Added test catering models, urls, and views

--- apps/catering/models.py
+++ apps/catering/models.py
@@ -1,6 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from ..users.models import User
 
-# Create your models here.
+
+# class Cart(models.Model):
+#     user_cart = models.OneToOneField('User')
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+class Order(models.Model):
+    # order_cart = models.('Cart')
+    order_user = models.ForeignKey(User)
+    made_order = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    # quantity = models.IntegerField()
+    order = models.ManyToManyField(Order, related_name='product')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+    product = models.ManyToManyField(Product, related_name='category')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- apps/catering/views.py
+++ apps/catering/views.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from .models import Order, Product, Category
+from ..users.models import User
 # Create your views here.
+
 def index(request):
-    pass
+    return render(request, 'catering:dashboard.html')
